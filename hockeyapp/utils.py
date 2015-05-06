433b2d821c888f3cf9a1cb7efbbf53ee4496f1d1
Sam Fonseca <sam@bypassmobile.com>
2015-05-06 00:32:39 -0500

    - Updated utils and dlhockapk flags

--- hockeyapp/utils.py
+++ hockeyapp/utils.py
@@ -10,14 +10,15 @@ def get_apps_by(app_property, value):
 def get_app_by(app_property, value):
     return get_apps_by(app_property, value)[0]
 
-def get_version(app_id, version):
+def get_version(app, version):
     """Returns a JSON object of the first version of the app where app_version['shortversion'].startswith(version) is
     true
 
-    :param app_id: The public_identifier of the target app as a string.
+    :param app: The target app as returned by get_app_by.
     :param version: The version number of the target app as a string.
     :return: A JSON object of the app version.
     """
+    app_id = app['public_identifier']
     versions = hockeyapi.versions(app_id)
-    return filter(lambda app_version: app_version['shortversion'].startswith(version), versions['app_versions'])[0]
+    return filter(lambda app_version: app_version['shortversion'].startswith(str(version)), versions['app_versions'])[0]
 
